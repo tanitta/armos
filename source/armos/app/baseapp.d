371692ea223b0901581b451384508d96aac34a32
tanitta <ttata.trit@gmail.com>
2015-11-05 08:10:20 +0900

    enable to work event.

--- source/armos/app/baseapp.d
+++ source/armos/app/baseapp.d
@@ -1,8 +1,26 @@
 module armos.app.baseapp;
+import armos.events.event;
 class BaseApp{
+	void setup(ref EventArg arg){
+		setup();
+	}
+	void update(ref EventArg arg){
+		update();
+	}
+	void draw(ref EventArg arg){
+		draw();
+	}
+	
 	void setup(){};
+	
 	void update(){};
+	
 	void draw(){};
-	// void keyDown(KeyEvent event){};
-	// void mouseDrag(MouseEvent event){};
+	
+	void keyPressed(ref KeyPressedEventArg m) {
+		keyPressed(m.str);
+	}
+	
+	void keyPressed(string str) {
+	}
 }
--- source/armos/events/event.d
+++ source/armos/events/event.d
@@ -1,27 +1,112 @@
 module armos.events.event;
 import core.sync.mutex;
+import std.array;
+import std.stdio;
+// class Function(ArgumentType){
+// 	private
+// 		void delegate(ArgumentType) func;
+// 	public
+// 		this(void delegate(ArgumentType) f){
+// 			func = f;
+// 		}
+// 	void run(ArgumentType arg){
+// 		func(arg);
+// 	}
+// }
 
-class Function{
-
-}
-class BaseEvent(Func) {
+class Event(ArgumentType) {
 	protected
-		Func[] functions;
-		bool enabled = true;
-		bool notifying = false;
-		auto mutex = new Mutex;
-		
-		void remove(void function(EventType)){};
-		void add(){};
-		
+		void delegate(ref ArgumentType)[] functions;
+	bool enabled = true;
+	bool notifying = false;
+	// Mutex mutex;
+	void remove(void delegate(ref ArgumentType) func){
+	};
+
+	void add(void delegate(ref ArgumentType) func){
+		functions~=func;
+	};
+
+	void notify(ref ArgumentType arg){
+		foreach (func; functions) {
+			func(arg);
+		}
+	};
+
 	public
 		this(){
-
+			// mutex = new Mutex;
 		};
 }
 
-void addListener(EventType, ArgumentType, ListenerClass)(
-		ref EventType event, ListenerClass* listener, void function(EventType) listenerFunc){
-	event.remove(listener, listenerFunc);
-	event.add(listener, listenerFunc);
+void addListener(EventType, ListenerClass, FunctionType)(ref EventType event, ref ListenerClass listener, FunctionType listenerFunc){
+	event.remove(listenerFunc);
+	event.add(listenerFunc);
 };
+void notifyEvent(EventType, ArgType)(ref EventType event, ref ArgType arg){
+	event.notify(arg);
+}
+
+class EventArg{}
+
+class KeyPressedEventArg : EventArg{
+	string str;
+};
+
+class CoreEvents {
+	private
+		EventArg voidEventArg;
+	public
+		Event!(EventArg) setup;
+	Event!(EventArg) update;
+	Event!(EventArg) draw;
+	Event!(KeyPressedEventArg) keyPressed;
+	this(){
+		setup = new Event!(EventArg);
+		update= new Event!(EventArg);
+		draw = new Event!(EventArg);
+		keyPressed = new Event!(KeyPressedEventArg);
+	}
+	void notifySetup(){
+		notifyEvent(setup, voidEventArg);
+	};
+
+	void notifyUpdate(){};
+	void notifyDraw(){};
+	void notifyKeyPressed(){
+		writeln("notifyKeyPressed");
+		auto obj = new KeyPressedEventArg;
+		// obj.a = 2;
+		notifyEvent(keyPressed, obj);
+
+	}
+}
+
+unittest{
+	import std.stdio;
+	import armos.app;
+	
+	auto core_events = new CoreEvents;
+
+	class Hoge : armos.app.BaseApp {
+		//i want to write like c++. the following is verbose...
+		alias armos.app.BaseApp.setup setup;
+		alias armos.app.BaseApp.update update;
+		alias armos.app.BaseApp.draw draw;
+		alias armos.app.BaseApp.keyPressed keyPressed;
+		
+		void setup(){}
+		void update(){}
+		void draw(){}
+
+		void keyPressed(string str) {
+			writeln(str);
+		}
+		
+	}
+	
+	auto hoge = new Hoge;
+	addListener(core_events.setup, hoge, &hoge.setup);
+	addListener(core_events.keyPressed, hoge, &hoge.keyPressed);
+	core_events.notifyKeyPressed();
+}
