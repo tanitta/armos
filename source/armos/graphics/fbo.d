9341a789aaad4cace0b0c761af7a82e03ef307d1
tanitta <ttata.trit@gmail.com>
2015-12-06 22:58:17 +0900

    enable to work Fbo initialization

--- source/armos/graphics/fbo.d
+++ source/armos/graphics/fbo.d
@@ -16,19 +16,17 @@ class Fbo{
 	}
 	
 	this(){
-		// glGenFramebuffers(1, &hoge);
-		// glGenFramebuffers(1, cast(uint*)&fboID_);
-		// colorRbo = new armos.graphics.Rbo;
-		// depthRbo = new armos.graphics.Rbo;
-		//
-		//
-		// glGetIntegerv(GL_FRAMEBUFFER_BINDING, &savedFboID_);
-		// glBindFramebuffer(GL_FRAMEBUFFER, fboID_);
-		// 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, colorRbo.id);
-		// 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRbo.id);
-		// glBindFramebuffer(GL_FRAMEBUFFER, fboID_);
-		//
-		// texture = new armos.graphics.Texture;
+		glGenFramebuffers(1, cast(uint*)&fboID_);
+		colorRbo = new armos.graphics.Rbo;
+		depthRbo = new armos.graphics.Rbo;
+		
+		glGetIntegerv(GL_FRAMEBUFFER_BINDING, &savedFboID_);
+		glBindFramebuffer(GL_FRAMEBUFFER, fboID_);
+			glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, colorRbo.id);
+			glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRbo.id);
+		glBindFramebuffer(GL_FRAMEBUFFER, fboID_);
+		
+		texture = new armos.graphics.Texture;
 	}
 	
 	void begin(){
@@ -49,14 +47,13 @@ class Fbo{
 						0, 0,
 						0, 0, armos.app.currentWindow.size[0], armos.app.currentWindow.size[1]
 				);
+				
 			texture.end;
 		end;
-		
 		//draw texture
 	}
 	
 	void draw(in armos.math.Vector2f position, in armos.math.Vector2f size){
 		draw(position[0], position[1], size[0], size[1]);
-	
 	}
 }
