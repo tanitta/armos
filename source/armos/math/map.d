785d5861f806e7d36e9f64441afe62296450bd31
tanitta <ttata.trit@gmail.com>
2015-12-18 15:38:35 +0900

    enable to set clamp in map

--- source/armos/math/map.d
+++ source/armos/math/map.d
@@ -11,12 +11,16 @@ pure T clamp(T)(in T v, in T min, in T max){
 	}
 }
 
-pure T map(T)(in T v1, in T v1_min, in T v1_max, in T v2_min, in T v2_max){
-	if(( v1_max - v1_min ).abs < T.epsilon){
-		return v2_min;
-	}else{
-		return (v1 - v1_min) / (v1_max - v1_min) * (v2_max - v2_min) + v2_min;
-	}
+pure T map(T)(in T v1, in T v1_min, in T v1_max, in T v2_min, in T v2_max, bool isClamp = true){
+		if(( v1_max - v1_min ).abs < T.epsilon){
+			return v2_min;
+		}else{
+			if(isClamp){
+				return clamp( (v1 - v1_min) / (v1_max - v1_min) * (v2_max - v2_min) + v2_min, v2_min, v2_max);
+			}else{
+				return (v1 - v1_min) / (v1_max - v1_min) * (v2_max - v2_min) + v2_min;
+			}
+		}
 }
 
 
@@ -24,5 +28,6 @@ unittest{
 	assert(0.5.map(0.0, 1.0, 1.0, 2.0) == 1.5);
 	assert(0.5.clamp(-0.5, 2.0) == 0.5);
 	assert((-1.0).clamp(-0.5, 2.0) == -0.5);
+	assert((3.0).clamp(-0.5, 2.0) == 2.0);
 
 }
