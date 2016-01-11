52cfb2acd4d982bb27587685bc6402c83387326c
tanitta <ttata.trit@gmail.com>
2016-01-12 01:19:31 +0900

    fix indent and add texture getter

--- source/armos/graphics/image.d
+++ source/armos/graphics/image.d
@@ -59,149 +59,153 @@ class Image {
 			Load image.
 
 			画像を読み込みます．
-			++/
-			void load(string pathInDataDir){
-				import std.string;
-				FIBITMAP * freeImageBitmap = null;
-				_bitmap = armos.graphics.Bitmap!(char)();
-				
-				string fileName = armos.utils.absolutePath(pathInDataDir);
+		++/
+		void load(string pathInDataDir){
+			import std.string;
+			FIBITMAP * freeImageBitmap = null;
+			_bitmap = armos.graphics.Bitmap!(char)();
+			
+			string fileName = armos.utils.absolutePath(pathInDataDir);
 
-				FREE_IMAGE_FORMAT freeImageFormat = FIF_UNKNOWN;
-				freeImageFormat = FreeImage_GetFileType(fileName.toStringz , 0);
+			FREE_IMAGE_FORMAT freeImageFormat = FIF_UNKNOWN;
+			freeImageFormat = FreeImage_GetFileType(fileName.toStringz , 0);
 
-				if(freeImageFormat == FIF_UNKNOWN) {
-					freeImageFormat = FreeImage_GetFIFFromFilename(fileName.toStringz);
-				}
-				if((freeImageFormat != FIF_UNKNOWN) && FreeImage_FIFSupportsReading(freeImageFormat)) {
-					freeImageBitmap = FreeImage_Load(freeImageFormat, fileName.toStringz, 0);
-					if (freeImageBitmap != null){
-						_isLoaded = true;
-					}
-				}
-
-				if ( _isLoaded ){
-					//TODO: bring freeImageBitmap to armos.graphics.Bitmap
-					bitmap(freeImageBitmap);
-				}
-
+			if(freeImageFormat == FIF_UNKNOWN) {
+				freeImageFormat = FreeImage_GetFIFFromFilename(fileName.toStringz);
+			}
+			if((freeImageFormat != FIF_UNKNOWN) && FreeImage_FIFSupportsReading(freeImageFormat)) {
+				freeImageBitmap = FreeImage_Load(freeImageFormat, fileName.toStringz, 0);
 				if (freeImageBitmap != null){
-					FreeImage_Unload(freeImageBitmap);
+					_isLoaded = true;
 				}
-
-				allocate;
 			}
+
+			if ( _isLoaded ){
+				//TODO: bring freeImageBitmap to armos.graphics.Bitmap
+				bitmap(freeImageBitmap);
+			}
+
+			if (freeImageBitmap != null){
+				FreeImage_Unload(freeImageBitmap);
+			}
+
+			allocate;
+		}
 
 		/++
 			Draw image data which loaded in advance.
 
 			読み込んだ画像データを画面に描画します．
-			++/
-			void draw(T)(in T x, in T y, in T z = T(0)){
-				if(_isLoaded){
-					armos.graphics.pushMatrix;
-					armos.graphics.translate(x, y, z);
-					_texture.begin;
-					_rect.drawFill();
-					_texture.end;
-					armos.graphics.popMatrix;
-				}
+		++/
+		void draw(T)(in T x, in T y, in T z = T(0)){
+			if(_isLoaded){
+				armos.graphics.pushMatrix;
+				armos.graphics.translate(x, y, z);
+				_texture.begin;
+				_rect.drawFill();
+				_texture.end;
+				armos.graphics.popMatrix;
 			}
+		}
 
 		/++
 			Draw image data which loaded in advance.
 
 			読み込んだ画像データを画面に描画します．
-			++/
-			void draw(T)(in T position)const{
-				static if(position.data.length == 2)
-					draw(position[0], position[1]);
-				else if(position.data.length == 3)
-					draw(position[0], position[1], position[2]);
-				else
-					static assert(0, "arg is invalid dimention");
-			}
+		++/
+		void draw(T)(in T position)const{
+			static if(position.data.length == 2)
+				draw(position[0], position[1]);
+			else if(position.data.length == 3)
+				draw(position[0], position[1], position[2]);
+			else
+				static assert(0, "arg is invalid dimention");
+		}
 
 		/++
 			Return image size.
 
 			画像のサイズを返します．
-			++/
-			armos.math.Vector2i size()const{return _size;}
+		++/
+		armos.math.Vector2i size()const{return _size;}
 
 		/++
 			Return width.
 
 			画像の幅を返します．
-			++/
-			int width()const{return _size[0];}
+		++/
+		int width()const{return _size[0];}
 
 		/++
 			Return height.
 
 			画像の高さを返します．
-			++/
-			int height()const{return _size[1];}
+		++/
+		int height()const{return _size[1];}
 
 		/++
 			Return bitmap pointer.
 
 			画像のビットマップデータを返します．
-			++/
-			armos.graphics.Bitmap!(char) bitmap(){return _bitmap;}
+		++/
+		armos.graphics.Bitmap!(char) bitmap(){return _bitmap;}
 
 		/++
 			Set bitmap
-			++/
-			void bitmap(armos.graphics.Bitmap!(char) data){
-				_bitmap = data;
-				allocate();
-				_isLoaded = true;
-			}
+		++/
+		void bitmap(armos.graphics.Bitmap!(char) data){
+			_bitmap = data;
+			allocate();
+			_isLoaded = true;
+		}
 
 		/++
 			Generate a image from the aligned pixels
 
 			一次元配列からImageを生成します
-			++/
-			void setFromAlignedPixels(T)(T* pixels, int width, int height, armos.graphics.ColorFormat format){
-				_bitmap.setFromAlignedPixels(cast(char*)pixels, width, height, format);
-				allocate;
-				_isLoaded = true;
-			}
+		++/
+		void setFromAlignedPixels(T)(T* pixels, int width, int height, armos.graphics.ColorFormat format){
+			_bitmap.setFromAlignedPixels(cast(char*)pixels, width, height, format);
+			allocate;
+			_isLoaded = true;
+		}
 
 		/++
 			与えられたbitmapを元にtextureとrectを生成します
-			++/
-			void allocate(){
-				_texture = new armos.graphics.Texture;
-				_texture.allocate(_bitmap);
-				_rect = new armos.graphics.Mesh;
-				_rect.primitiveMode = armos.graphics.PrimitiveMode.Quads;
-				float x = _bitmap.width;
-				float y = _bitmap.height;
+		++/
+		void allocate(){
+			_texture = new armos.graphics.Texture;
+			_texture.allocate(_bitmap);
+			_rect = new armos.graphics.Mesh;
+			_rect.primitiveMode = armos.graphics.PrimitiveMode.Quads;
+			float x = _bitmap.width;
+			float y = _bitmap.height;
 
-				_texture.begin;
-				_rect.addTexCoord(0, 0);_rect.addVertex(0, 0, 0);
-				_rect.addTexCoord(1.0*_bitmap.height/_texture.height, 0);_rect.addVertex(0, y, 0);
-				_rect.addTexCoord(1.0*_bitmap.height/_texture.height, 1.0);_rect.addVertex(x, y, 0);
-				_rect.addTexCoord(0, 1.0);_rect.addVertex(x, 0, 0);
-				_texture.end;
+			_texture.begin;
+			_rect.addTexCoord(0, 0);_rect.addVertex(0, 0, 0);
+			_rect.addTexCoord(1.0*_bitmap.height/_texture.height, 0);_rect.addVertex(0, y, 0);
+			_rect.addTexCoord(1.0*_bitmap.height/_texture.height, 1.0);_rect.addVertex(x, y, 0);
+			_rect.addTexCoord(0, 1.0);_rect.addVertex(x, 0, 0);
+			_texture.end;
 
-				_rect.addIndex(0);
-				_rect.addIndex(1);
-				_rect.addIndex(2);
-				_rect.addIndex(3);
-			}
+			_rect.addIndex(0);
+			_rect.addIndex(1);
+			_rect.addIndex(2);
+			_rect.addIndex(3);
+		}
 
 		/++
 			Retun true if the image was loaded.
 
 			画像が読み込まれている場合trueを，そうでない場合はfalseを返します．
-			++/
-			bool isLoaded(){
-				return _isLoaded;
-			}
+		++/
+		bool isLoaded(){
+			return _isLoaded;
+		}
+		
+		armos.graphics.Texture texture(){
+			return _texture;
+		}
 	}//public
 
 	private{
