99c7863383ab1b1b21a1e36da8bd6022e35104e2
tanitta <ttata.trit@gmail.com>
2016-02-29 01:56:49 +0900

    Add Color cast.

--- source/armos/types/color.d
+++ source/armos/types/color.d
@@ -2,129 +2,147 @@ module armos.types.color;
 import armos.math;
 
 /++
-色情報を表すstructを構成するtemplateです．
 +/
-mixin template BaseColor(ColorType, PixelType) {
-	static const PixelType limit = 255;
-	PixelType r=limit;
-	PixelType g=limit;
-	PixelType b=limit; 
-	PixelType a=limit;
-	// this(){
-	// 	r = PixelType.max;
-	// 	g = PixelType.max;
-	// 	b = PixelType.max;
-	// 	a = PixelType.max;
-	// }
+struct BaseColor(T){
+	alias C = BaseColor!(T);
 	
-	/++
-		16進数のカラーコードで色を指定します．
-	+/
-	this(int hexColor, float alpha = limit){
-		r = (hexColor >> 16) & 0xff;
-		g = (hexColor >> 8) & 0xff;
-		b = (hexColor >> 0) & 0xff;
-		a = cast(PixelType)alpha;
-	}
-	
-	/++
-		RGBAで色を指定します．透明度は省略可能です．
-	+/
-	this(float red, float green, float blue, float alpha = limit){
-		r = armos.math.clamp(cast(PixelType)red, cast(PixelType)0, limit);
-		g = armos.math.clamp(cast(PixelType)green, cast(PixelType)0, limit);
-		b = armos.math.clamp(cast(PixelType)blue, cast(PixelType)0, limit);
-		a = armos.math.clamp(cast(PixelType)alpha, cast(PixelType)0, limit);
-	}
-	
-	/++
-		色の加算を行います．
-	+/
-	ColorType opAdd(ColorType color){
-		ColorType result = ColorType();
-		result.r = cast(PixelType)( this.r * this.a + color.r * color.a );
-		result.g = cast(PixelType)( this.g * this.a + color.g * color.a );
-		result.b = cast(PixelType)( this.b * this.a + color.b * color.a );
-		// result.a = cast(PixelType)( this.a + color.a );
-		return result;
-	}
-	
-	/++
-		色の減算を行います．
-	+/
-	ColorType opSub(ColorType color){
-		ColorType result = ColorType();
-		result.r = cast(PixelType)( this.r * this.a - color.r * color.a );
-		result.g = cast(PixelType)( this.g * this.a - color.g * color.a );
-		result.b = cast(PixelType)( this.b * this.a - color.b * color.a );
-		// result.a = cast(PixelType)( this.a - color.a );
-		return result;
-	}
-	
-	/++
-		色をHSBで指定します．
-	+/
-	ColorType hsb(in PixelType hue, in PixelType saturation, in PixelType value){
-		import std.math;
-		float castedLimit = cast(float)limit;
-		float h = cast(float)hue*360.0/castedLimit;
-		int hi = cast(int)( floor(h / 60.0f) % 6 );
-		auto f  = (h / 60.0f) - floor(h / 60.0f);
-		auto p  = round(value * (1.0f - (saturation / castedLimit)));
-		auto q  = round(value * (1.0f - (saturation / castedLimit) * f));
-		auto t  = round(value * (1.0f - (saturation / castedLimit) * (1.0f - f)));
-		float red, green, blue;
-		switch (hi) {
-			case 0:
-				red = value, green = t,     blue = p;
-				break;
-			case 1:
-				red = q,     green = value, blue = p;
-				break;
-			case 2:
-				red = p,     green = value, blue = t;
-				break;
-			case 3:
-				red = p,     green = q,     blue = value;
-				break;
-			case 4:
-				red = t,     green = p,     blue = value;
-				break;
-			case 5:
-				red = value, green = p,     blue = q;
-				break;
-			default:
-				break;
+	public{
+		static if(is(T == char)){
+			enum T limit = 255;
+		}else static if(is(T == float)){
+			enum T limit = 1.0;
 		}
-		r = cast(PixelType)red;
-		g = cast(PixelType)green;
-		b = cast(PixelType)blue;
-		return this;
-	}
-	
-}
+		
+		T r = limit;
+		T g = limit;
+		T b = limit; 
+		T a = limit;
+
+		/++
+		16進数のカラーコードで色を指定します．
+		+/
+		this(int hexColor, float alpha = limit){
+			r = (hexColor >> 16) & 0xff;
+			g = (hexColor >> 8) & 0xff;
+			b = (hexColor >> 0) & 0xff;
+			a = cast(T)alpha;
+		}
+
+		/++
+		RGBAで色を指定します．透明度は省略可能です．
+		+/
+		this(float red, float green, float blue, float alpha = limit){
+			r = armos.math.clamp(cast(T)red, cast(T)0, limit);
+			g = armos.math.clamp(cast(T)green, cast(T)0, limit);
+			b = armos.math.clamp(cast(T)blue, cast(T)0, limit);
+			a = armos.math.clamp(cast(T)alpha, cast(T)0, limit);
+		}
+
+		/++
+		色の加算を行います．
+		+/
+		C opAdd(C color){
+			C result = C();
+			result.r = cast(T)( this.r * this.a + color.r * color.a );
+			result.g = cast(T)( this.g * this.a + color.g * color.a );
+			result.b = cast(T)( this.b * this.a + color.b * color.a );
+			// result.a = cast(T)( this.a + color.a );
+			return result;
+		}
+
+		/++
+		色の減算を行います．
+		+/
+		C opSub(C color){
+			C result = C();
+			result.r = cast(T)( this.r * this.a - color.r * color.a );
+			result.g = cast(T)( this.g * this.a - color.g * color.a );
+			result.b = cast(T)( this.b * this.a - color.b * color.a );
+			// result.a = cast(T)( this.a - color.a );
+			return result;
+		}
+
+		/++
+		色をHSBで指定します．
+		+/
+		C hsb(in T hue, in T saturation, in T value){
+			import std.math;
+			float castedLimit = cast(float)limit;
+			float h = cast(float)hue*360.0/castedLimit;
+			int hi = cast(int)( floor(h / 60.0f) % 6 );
+			auto f  = (h / 60.0f) - floor(h / 60.0f);
+			auto p  = round(value * (1.0f - (saturation / castedLimit)));
+			auto q  = round(value * (1.0f - (saturation / castedLimit) * f));
+			auto t  = round(value * (1.0f - (saturation / castedLimit) * (1.0f - f)));
+			float red, green, blue;
+			switch (hi) {
+				case 0:
+					red = value, green = t,     blue = p;
+					break;
+				case 1:
+					red = q,     green = value, blue = p;
+					break;
+				case 2:
+					red = p,     green = value, blue = t;
+					break;
+				case 3:
+					red = p,     green = q,     blue = value;
+					break;
+				case 4:
+					red = t,     green = p,     blue = value;
+					break;
+				case 5:
+					red = value, green = p,     blue = q;
+					break;
+				default:
+					break;
+			}
+			r = cast(T)red;
+			g = cast(T)green;
+			b = cast(T)blue;
+			return this;
+		}
+		
+		F opCast(F)()const if(is(F == BaseColor!(typeof( F.r )))){
+			import std.conv:to;
+			F castedColor= F(0, 0, 0, 0);
+			float c = cast(float)castedColor.limit / cast(float)limit;
+			alias CT = typeof(F.r);
+			castedColor.r = cast(CT)( cast(float)r/c );
+			castedColor.g = cast(CT)( cast(float)g/c );
+			castedColor.b = cast(CT)( cast(float)b/c );
+			castedColor.a = cast(CT)( cast(float)a/c );
+			return castedColor;
+		}
+	}//public
+
+	private{
+	}//private
+}//struct BaseColor
 
 /++
 色を表すstructです．1byteの色深度を持ちます．
 +/
-struct Color{
-	mixin BaseColor!(Color, char);
-	static const char limit = 255;
-	// this(float red, float green, float blue, float alpha = limit){
-		// this(red, green, blue, alpha);
-	// }
-};
+alias BaseColor!(char) Color;
 
 /++
 色を表すstructです．浮動小数点型で値を持ちます．
 
 最小値は0.0，最大値は1.0です．
 +/
-struct FloatColor{
-	mixin BaseColor!(FloatColor, float);
-	static const float limit = 1.0;
-};
+alias BaseColor!(float) FloatColor;
+// struct FloatColor{
+// 	mixin BaseColor!(FloatColor, float);
+// 	static const float limit = 1.0;
+// };
 
 unittest{
 	auto color = Color();
+}
+unittest{
+	import std.stdio;
+	import std.conv:to;
+	auto color = Color(150, 2, 255);
+	// FloatColor fColor = color.to!FloatColor;
+	// color.hoge;
 }
