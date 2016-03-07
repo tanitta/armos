1572e00be05a43b11ef4a12052f50484a4df8d37
tanitta <ttata.trit@gmail.com>
2016-03-07 23:42:13 +0900

    Add timeline.

--- source/armos/utils/package.d
+++ source/armos/utils/package.d
@@ -2,4 +2,5 @@ module armos.utils;
 public import armos.utils.fpscounter;
 public import armos.utils.file;
 public import armos.utils.gui;
+public import armos.utils.timeline;
 
---
+++ source/armos/utils/timeline.d
@@ -0,0 +1,1 @@
+module armos.utils.timeline;
