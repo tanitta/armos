7e2dd0473e61c007a4200163f6aa195d4620334b
tanitta <ttata.trit@gmail.com>
2016-05-11 23:21:05 +0900

    Fix format of fpscounter.

--- source/armos/utils/fpscounter.d
+++ source/armos/utils/fpscounter.d
@@ -1,49 +1,67 @@
 module armos.utils.fpscounter;
 import core.time;
 import core.thread;
+import std.conv;
+
 class FpsCounter {
-	private double targetFps_ = 60.0;
-	private double currentFps = 60.0;
-	private ulong  nFrameCount = 0;
-	private int targetTime = 0;
-	private MonoTime timer;
-	double fpsUseRate = 0;
-	this(double targetFps_ = 60){
-		timer = MonoTime.currTime;
-		this.targetFps_ = targetFps_;
-		this.currentFps= targetFps_;
-		targetTime = cast(int)(1.0/targetFps_*10000000);
-	}
-	
-	void targetFps(double fps){
-		targetFps_ = fps;
-		targetTime = cast(int)(1.0/targetFps_*10000000);
-	}
-
-	double getTargetFps(){
-		return targetFps_;
-	}
-	double getCurrentFps(){
-		return currentFps;
-	}
-
-	void newFrame(){
-		timer = MonoTime.currTime;
-		nFrameCount++;
-	}
-	
-	ulong getNumFrames(){return nFrameCount;};
-
-	void adjust(){
-		MonoTime after = MonoTime.currTime;
-		auto def = ( after - timer );
-		if( def.fracSec.hnsecs < targetTime){
-			Thread.sleep( dur!("hnsecs")( targetTime - def.fracSec.hnsecs ) );
+	public{
+		this(in double defaultFps = 60){
+			_timer = MonoTime.currTime;
+			_currentFps = defaultFps;
+			this.targetFps = defaultFps;
 		}
-		MonoTime after2 = MonoTime.currTime;
-		auto def2 = after2 - timer;
-		currentFps = 1.0/cast(double)(def2.fracSec.hnsecs)*10000000;
-		fpsUseRate = cast(double)def.fracSec.hnsecs/cast(double)targetTime;
+
+		void targetFps(in double fps){
+			_targetFps = fps;
+			_targetTime = convertedTimeFromFps(fps);
+		}
+
+		double targetFps()const{
+			return _targetFps;
+		}
+		
+		double currentFps()const{
+			return _currentFps;
+		}
+
+		void newFrame(){
+			_timer = MonoTime.currTime;
+			_currentFrames++;
+		}
+
+		ulong currenFrames()const{
+			return _currentFrames;
+		};
+
+		double fpsUseRate()const{
+			return _fpsUseRate;
+		}
+		
+		void adjust(){
+			immutable MonoTime after = MonoTime.currTime;
+			immutable def = ( after - _timer );
+			if( def.fracSec.hnsecs < _targetTime){
+				Thread.sleep( dur!("hnsecs")( _targetTime - def.fracSec.hnsecs ) );
+			}
+			immutable after2 = MonoTime.currTime;
+			immutable def2 = after2 - _timer;
+			_currentFps = 1.0/def2.fracSec.hnsecs.to!double*10000000.0;
+			_fpsUseRate = def.fracSec.hnsecs.to!double/_targetTime.to!double;
+		}
 	}
+	
+	private{
+		double _targetFps = 60.0;
+		double _currentFps = 60.0;
+		ulong  _currentFrames = 0;
+		int _targetTime = 0;
+		double _fpsUseRate = 0;
+		MonoTime _timer;
+		
+	}
+}
+
+private int convertedTimeFromFps(in double fps){
+	return (1.0/fps*10000000.0).to!int;
 }
 
