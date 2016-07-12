fa233aa0b9aa90984b8dbf27dec6ba287cc753ce
tanitta <ttata.trit@gmail.com>
2016-07-12 16:06:15 +0900

    Add ScopedModelMatrix.

--- source/armos/graphics/scopes.d
+++ source/armos/graphics/scopes.d
@@ -1,1 +1,28 @@
 module armos.graphics.scopes;
+
+import armos.math.matrix:Matrix4f;
+
+/++
++/
+struct ScopedModelMatrix {
+    public{
+        this(in Matrix4f m){begin;}
+        ~this(){end;}
+    }//public
+
+    private{
+        import std.stdio;
+        void begin(){"begin".writeln;}
+        void end(){"end".writeln;}
+    }//private
+}//struct ScopedModelMatrix
+
+ScopedModelMatrix scopedModelMatrix(in Matrix4f m = Matrix4f.identity){
+    return ScopedModelMatrix(m);
+}
+
+unittest{
+    // import std.stdio;
+    // auto s = scopedModelMatrix();
+    // "hoge".writeln;
+}
