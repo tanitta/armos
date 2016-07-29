001be65b8e3c8e9d01b3b4eb616d192ed8b64a37
tanitta <ttata.trit@gmail.com>
2016-07-29 16:05:13 +0900

    Move scoped module.

---
+++ source/armos/utils/scoped.d
@@ -0,0 +1,23 @@
+module armos.utils.scoped;
+
+/++
++/
+struct Scoped(T){
+    public{
+        this(T t){
+            _t = t;
+            _t.begin;
+        }
+        ~this(){
+            _t.end;
+        }
+    }//public
+
+    private{
+        T _t;
+    }//private
+}//struct ScopedShader
+
+Scoped!T scoped(T)(T t = new T()){
+    return Scoped!T(t);
+}
