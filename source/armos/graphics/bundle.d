7adf21ae4c59707d5cff86ca0015d7d3158682f1
tanitta <ttata.trit@gmail.com>
2016-08-17 23:40:58 +0900

    Remove vao from Bundle to BufferMesh.

--- source/armos/graphics/bundle.d
+++ source/armos/graphics/bundle.d
@@ -1,6 +1,8 @@
 module armos.graphics.bundle;
 
 import armos.graphics.vao;
+import armos.graphics.buffer;
+import armos.graphics.buffermesh;
 import armos.graphics.shader;
 import armos.graphics.mesh;
 import armos.utils.scoped;
@@ -9,39 +11,56 @@ import armos.utils.scoped;
 +/
 class Bundle {
     public{
-        this(){
-            _vao = new Vao();
+        ///
+        this(Mesh mesh, Shader shader, in BufferUsageFrequency freq, in BufferUsageNature nature){
+            this(new BufferMesh(mesh, freq, nature), shader);
         }
         
+        ///
+        this(BufferMesh bufferMesh, Shader shader){
+            _shader = shader;
+            _bufferMesh = bufferMesh;
+            
+            //TODO bind attribs to shader.
+            //e.g.
+            // attribs["vertices"].begin;
+            // _shader.setAttrib("coord");
+            // attribs["vertices"].end;
+        }
+        
+        ///
         Bundle shader(Shader shader){
             _shader = shader;
             return this;
         }
         
-        Bundle mesh(Mesh mesh){
-            _mesh = mesh;
+        ///
+        Bundle mesh(Mesh mesh, in BufferUsageFrequency freq, in BufferUsageNature nature){
+            _bufferMesh = new BufferMesh(mesh, freq, nature);
             return this;
         }
         
+        ///
         void begin(){
             _shader.begin;
-            _vao.begin;
+            _bufferMesh.vao.begin;
         }
         
+        ///
         void end(){
-            _vao.end;
+            _bufferMesh.vao.end;
             _shader.end;
         }
         
+        ///
         void draw(){
+            auto scopedVao    = scoped(_bufferMesh.vao);
             auto scopedShader = scoped(_shader);
-            auto scopedVao    = scoped(_vao);
         }
     }//public
 
     private{
-        Vao _vao;
         Shader _shader;
-        Mesh _mesh;
+        BufferMesh _bufferMesh;
     }//private
 }//class Bundle
