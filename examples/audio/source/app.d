8d80a5c963beb222194e0e19575b58e980d67018
tanitta <ttata.trit@gmail.com>
2016-08-26 20:29:01 +0900

    Fix package identifier in audio example.

--- examples/audio/source/app.d
+++ examples/audio/source/app.d
@@ -1,13 +1,9 @@
-import armos.app;
-import armos.audio;
-import armos.math;
-alias ar = armos;
+static import ar = armos;
 import std.stdio;
 
 class TestApp : ar.app.BaseApp{
     override void setup(){
         _player = new ar.audio.Player;
-        
         
         _buffer = (new ar.audio.Buffer).load("data/bgm.ogg")// loading a BGM(Music provided by OtObOx.thank you!)
                                        .range(0f, 10f);
