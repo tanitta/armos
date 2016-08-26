be9dc25df818f7c57b8afdd2b70b4c27d42d324f
tanitta <ttata.trit@gmail.com>
2016-08-26 21:20:25 +0900

    Fix package identifier in image example.

--- examples/image/source/app.d
+++ examples/image/source/app.d
@@ -1,43 +1,45 @@
-import armos;
+static import ar = armos;
 
-class TestApp : ar.BaseApp{
-	ar.Image imageLena;
-	ar.Image imageDman;
-	ar.Image imageFromArray;
-	
-	void setup(){
-		ar.blendMode = ar.BlendMode.Alpha;
+class TestApp : ar.app.BaseApp{
+	override void setup(){
+		ar.graphics.blendMode = ar.graphics.BlendMode.Alpha;
 		
-		imageLena = new ar.Image;
+		imageLena = new ar.graphics.Image;
 		imageLena.load("lena_std.tif");
 		
-		imageDman= new ar.Image;
+		imageDman= new ar.graphics.Image;
 		imageDman.load("d-man.png");
-		imageDman.setMinMagFilter(ar.TextureFilter.Nearest);
+		imageDman.setMinMagFilter(ar.graphics.TextureFilter.Nearest);
 		
-		imageFromArray = new ar.Image;
+		imageFromArray = new ar.graphics.Image;
 		ubyte[100*100] array;
-		ar.Vector2i position;
-		auto center = ar.Vector2i(50, 50);
+		ar.math.Vector2i position;
+		auto center = ar.math.Vector2i(50, 50);
 		foreach (int index, ref pixel; array) {
 			position[0] = index%100;
 			position[1] = index/100;
 			pixel = 255 - cast(ubyte)( ( position-center ).norm*20 );
 		}
-		imageFromArray.setFromAlignedPixels(array.ptr, 100, 100, ar.ColorFormat.Gray);
+		imageFromArray.setFromAlignedPixels(array.ptr, 100, 100, ar.graphics.ColorFormat.Gray);
 	}
 	
-	void draw(){
+	override void draw(){
 		imageLena.drawCropped(512, 512, 256, 256, 512, 512);
 		imageLena.draw(0, 0);
 		
-		ar.pushMatrix;
-			ar.scale(10, 10, 1);
+		ar.graphics.pushMatrix;
+			ar.graphics.scale(10, 10, 1);
 			imageDman.draw(14, 5);
-		ar.popMatrix;
+		ar.graphics.popMatrix;
 		
 		imageFromArray.draw(300, 300);
 	}
+    
+    private{
+        ar.graphics.Image imageLena;
+        ar.graphics.Image imageDman;
+        ar.graphics.Image imageFromArray;
+    }
 }
 
-void main(){ar.run(new TestApp);}
+void main(){ar.app.run(new TestApp);}
