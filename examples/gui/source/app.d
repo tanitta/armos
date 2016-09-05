486eceeab37bc77b110eb64b146931c82ddde313
namachan10777 <namachan10777@gmail.com>
2016-09-05 22:01:42 +0900

    fix gui example

--- examples/gui/source/app.d
+++ examples/gui/source/app.d
@@ -1,9 +1,11 @@
-import armos, std.stdio, std.math, std.conv;
+import std.stdio, std.math, std.conv;
 
-class TestApp : ar.BaseApp{
-	ar.Camera camera = new ar.Camera();
-	ar.Gui gui;
-	ar.Mesh mesh;
+static import ar = armos;
+
+class TestApp : ar.app.BaseApp{
+	ar.graphics.Camera camera = new ar.graphics.Camera();
+	ar.utils.Gui gui;
+	ar.graphics.Mesh mesh;
 	float _fpsUseRate = 0.0;
 	float f = 128;
 	int i = 128;
@@ -12,59 +14,59 @@ class TestApp : ar.BaseApp{
 	float cY = 0;
 	bool isFill = false;
 	
-	void setup(){
-		ar.blendMode(ar.BlendMode.Alpha);
-		camera.position = ar.Vector3f(0, 0, -100);
-		camera.target= ar.Vector3f(0, 0, 0);
 		
-		gui = (new ar.Gui)
+	override void setup(){
+		ar.graphics.blendMode(ar.graphics.BlendMode.Alpha);
+		camera.position = ar.math.Vector3f(0, 0, -100);
+		camera.target= ar.math.Vector3f(0, 0, 0);
+		gui = (new ar.utils.Gui)
 		.add(
-			(new ar.List)
-			.add(new ar.Partition(" "))
-			.add(new ar.Label("armos-gui-example"))
-			.add(new ar.Partition)
+			(new ar.utils.List)
+			.add(new ar.utils.Partition(" "))
+			.add(new ar.utils.Label("armos-gui-example"))
+			.add(new ar.utils.Partition)
 		)
 		.add(
-			(new ar.List)
-			.add(new ar.Partition(" "))
-			.add(new ar.Label("list2"))
-			.add(new ar.Partition)
-			.add(new ar.MovingGraph!float("fpsUseRate", _fpsUseRate, 0.0, 100.0))
-			.add(new ar.Partition)
-			.add(new ar.Slider!int("slider!int", i, 0, 255))
-			.add(new ar.Slider!float("slider!float", f, 0, 255))
-			.add(new ar.Partition)
-			.add(new ar.MovingGraph!float("x", cX, -2, 2))
-			.add(new ar.MovingGraphXY!float("x", cX, -2, 2, "y", cY, -2, 2))
-			.add(new ar.ToggleButton("isFill", isFill))
-			.add(new ar.ToggleButton("isFill", isFill))
-			.add(new ar.Partition)
+			(new ar.utils.List)
+			.add(new ar.utils.Partition(" "))
+			.add(new ar.utils.Label("list2"))
+			.add(new ar.utils.Partition)
+			.add(new ar.utils.MovingGraph!float("fpsUseRate", _fpsUseRate, 0.0, 100.0))
+			.add(new ar.utils.Partition)
+			.add(new ar.utils.Slider!int("slider!int", i, 0, 255))
+			.add(new ar.utils.Slider!float("slider!float", f, 0, 255))
+			.add(new ar.utils.Partition)
+			.add(new ar.utils.MovingGraph!float("x", cX, -2, 2))
+			.add(new ar.utils.MovingGraphXY!float("x", cX, -2, 2, "y", cY, -2, 2))
+			.add(new ar.utils.ToggleButton("isFill", isFill))
+			.add(new ar.utils.ToggleButton("isFill", isFill))
+			.add(new ar.utils.Partition)
 		);
 		
-		mesh = ar.boxPrimitive(ar.Vector3f(0, 0, 0), ar.Vector3f( 100, 10, 10 ));
+		mesh = ar.graphics.boxPrimitive(ar.math.Vector3f(0, 0, 0), ar.math.Vector3f( 100, 10, 10 ));
 	}
 	
-	void update(){
-		_fpsUseRate = ar.fpsUseRate*100.0;
+	override void update(){
+		_fpsUseRate = ar.app.fpsUseRate*100.0;
 		c += 0.2;
 		cX = cos(c*f/128f);
 		cY = sin(c);
 	}
 	
-	void draw(){
+	override void draw(){
 		camera.begin;
-		ar.pushMatrix;
-		ar.rotate(f, 1, 1, 1);
-			ar.setColor(i, 255-i, i*0.5);
+		ar.graphics.pushMatrix;
+		ar.graphics.rotate(f, 1, 1, 1);
+			ar.graphics.color(i, 255-i, i*0.5);
 			if(isFill){
 				mesh.drawFill;
 			}else{
 				mesh.drawWireFrame;
 			}
-		ar.popMatrix;
+		ar.graphics.popMatrix;
 		camera.end;
 		gui.draw;
 	}
 }
 
-void main(){ar.run(new TestApp);}
+void main(){ar.app.run(new TestApp);}
