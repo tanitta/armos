1cab3961f425ed8239dd353959bc2a50702938ff
tanitta <ttata.trit@gmail.com>
2016-10-31 23:10:59 +0900

    Split analyzing functions from buffer.

--- source/armos/audio/buffer.d
+++ source/armos/audio/buffer.d
@@ -7,15 +7,7 @@ import derelict.vorbis.enc;
 import derelict.vorbis.file;
 
 import armos.audio.spectrum;
-
-/++
-+/
-enum BpmDetectionSource {
-    Beats,
-    // Tones
-    Volumes, 
-}//enum BpmDetectionSource
-
+import armos.audio.spectrumanalyzer:BpmDetectionSource;
 /++
 +/
 class Buffer {
@@ -144,7 +136,7 @@ class Buffer {
         int id()const{return _id;}
         
         ///
-        int samplingRate()const{
+        size_t samplingRate()const{
             return _sampleRate;
         }
         
@@ -185,7 +177,7 @@ class Buffer {
                          _format, 
                          rangedPcm.ptr,
                          rangedPcm.length.to!int*2,
-                         _sampleRate);
+                         _sampleRate.to!int);
             return this;
         }
         
@@ -204,17 +196,17 @@ class Buffer {
                 }
             }
             assert(result.length == bufferSize*2);
-            return analyze!R(result, _sampleRate);
+            import armos.audio.spectrumanalyzer;
+            return analyzeSpectrum!R(result, _sampleRate);
         }
         
         ///
-        auto detectBpm(in size_t bufferSize, in BpmDetectionSource sourceType = BpmDetectionSource.Beats)
+        Buffer detectBpm(in size_t bufferSize, in BpmDetectionSource sourceType = BpmDetectionSource.Beats)
         in{
             assert(0<channelLength);
         }
         body{
             alias F = double;
-            
             //Merge channels.
             F[] mergedSample = new F[](channelLength);
             for (int i = 0; i < channelLength; i++) {
@@ -224,89 +216,11 @@ class Buffer {
                 }
                 mergedSample[i] = sum;
             }
-            assert(0<mergedSample.length);
             
-            //Calc difference between current frame and previous one.
-            F[] differedSample = new F[](channelLength);
-            differedSample[0] = 0;
-            for (int i = 1; i < channelLength; i++) {
-                import std.algorithm:max;
-                differedSample[i] = max(0.0, mergedSample[i] - mergedSample[i-1]);
-            }
-            
-            //Calculate each frame spectrums.
-            Spectrum!F[] spectrums = new Spectrum!F[](channelLength/bufferSize);
-            for (int i = 0; i < channelLength/bufferSize; i++) {
-                import std.stdio;
-                import std.conv:to;
-                writeln("analyzing... ", (i*bufferSize).to!F/channelLength.to!F*100f, "%");
-                
-                F[] bufferSizedsample = new F[](bufferSize*2);
-
-                for (size_t j = 0; j < bufferSize*2; j++) {
-                    size_t k = 0;
-                    if(j + i*bufferSize < bufferSize*2){
-                        bufferSizedsample[j] = 0;
-                    }else{
-                        k = j + i*bufferSize - bufferSize*2;
-                        bufferSizedsample[j] = differedSample[k];
-                    }
-                }
-                
-                spectrums[i] = analyze!F(bufferSizedsample, _sampleRate);
-            }
-            
-            //Select tone range by sourceType.
-            auto filteredSample = new F[](channelLength/bufferSize); 
-            switch (sourceType) {
-                case BpmDetectionSource.Beats:
-                    foreach (size_t i, ref s; spectrums) {
-                        import std.algorithm:sum;
-                        filteredSample[i] = s.powers[0..$/108].sum;
-                        import std.math;
-                        assert(!isNaN(filteredSample[i]));
-                    }
-                    break;
-                    
-                case BpmDetectionSource.Volumes:
-                    foreach (size_t i, ref s; spectrums) {
-                        import std.algorithm:sum;
-                        filteredSample[i] = s.powers[0..$].sum;
-                        import std.math;
-                        assert(!isNaN(filteredSample[i]));
-                    }
-                    break;
-                default:
-                    assert(false);
-            }
-            
-            auto bpmSpectrum = analyze!F(filteredSample, _sampleRate/bufferSize);
-            
-            //filter by common bpm range.
-            auto filteredSpectrum = Spectrum!F();
-            {
-                auto r = bpmSpectrum.frequencyRange.arrayRange!("60<=a*60", "a*60<=150");
-                immutable first = r[0];
-                immutable last= r[1]+1;
-                filteredSpectrum.samplingRate = bpmSpectrum.samplingRate;
-                filteredSpectrum.frequencyRange = bpmSpectrum.frequencyRange[first..last];
-                filteredSpectrum.powers = bpmSpectrum.powers[first..last];
-                filteredSpectrum.phases = bpmSpectrum.phases[first..last];
-            }
-            
-            //Sort by power.
-            import std.range:zip;
-            import std.algorithm:sort;
-            zip(filteredSpectrum.frequencyRange, filteredSpectrum.powers, filteredSpectrum.phases).sort!"a[1]>b[1]";
-            
-            //Set bpm.
-            _bpm = filteredSpectrum.frequencyRange[0]*60.0;
-            
-            //Set phase.
-            immutable theta = filteredSpectrum.phases[0];
-            import std.math:PI;
-            _phase = ((theta<0.0)?theta+2.0*PI:theta)/(2.0*PI*filteredSpectrum.frequencyRange[0]);
-            
+            import armos.audio.spectrumanalyzer;
+            immutable t = analyzeBpmAndPhase!double(mergedSample, _sampleRate, bufferSize, sourceType);
+            _bpm = t[0];
+            _phase= t[1];
             return this;
         }
         
@@ -325,7 +239,7 @@ class Buffer {
         int _id;
         short[] _pcm;
         short[][] _channels;
-        int _sampleRate;
+        size_t _sampleRate;
         int _numChannels;
         int _depth;
         float _start;
@@ -363,68 +277,6 @@ class Buffer {
 }//class Buffer
 
 private{
-    version(LDC){
-        size_t nextPow2(size_t val) pure nothrow @nogc @safe
-        {
-            size_t res = 1;
-            while (res < val)
-                res <<= 1;
-            return res;
-        }
-    }
-    
-    Spectrum!R analyze(R, T)(T[] sample, in size_t samplingRate)
-    in{
-        import std.math:isNaN;
-        import std.conv;
-        foreach (size_t i, ref e; sample) {
-            assert(!isNaN(e), i.to!string);
-        }
-    }
-    out(spectrum){
-        import std.math:isNaN;
-        foreach (e; spectrum.powers) {
-            assert(!isNaN(e));
-        }
-        foreach (e; spectrum.phases) {
-            assert(!isNaN(e));
-        }
-        foreach (e; spectrum.frequencyRange) {
-            assert(!isNaN(e));
-        }
-    }
-    body{
-        import std.numeric:fft;
-        import std.range:iota;
-        import std.algorithm:map, fill;
-        import std.array:array;
-        import std.conv:to;
-        version(DigitalMars){
-            import std.math:nextPow2;
-        }
-        
-        if((sample.length-1).nextPow2 != sample.length){
-            auto zeroSlice = new T[](sample.length.nextPow2 - sample.length);
-            zeroSlice.fill(T(0));
-            sample = zeroSlice ~ sample;
-        }
-        
-        immutable windowLength = sample.length;
-        immutable transformLength = windowLength;//TODO
-        
-        auto frequencyRange = transformLength.iota.map!(x=>x.to!float*(samplingRate.to!float/transformLength.to!float).to!R)[0..$/2].array;
-        
-        const fx = fft(sample)[0..$/2];
-
-        Spectrum!R powerSpectrum;
-
-        powerSpectrum.samplingRate = samplingRate;
-        powerSpectrum.powers = fx.map!(x=>(x*typeof(x)(x.re, -x.im)).re/transformLength).array;
-        import std.math:atan2;
-        powerSpectrum.phases = fx.map!(x=>atan2(x.im, x.re)).array;
-        powerSpectrum.frequencyRange = frequencyRange;
-        return powerSpectrum;
-    }
     
     ALenum formatFrom(size_t numChannels, size_t depth){
         ALenum f;
@@ -564,12 +416,4 @@ private{
         WaveFileFormat format;
     }
 
-    import std.typecons:Tuple;
-    Tuple!(size_t, size_t) arrayRange(string Cl, string Cu, T)(in T[] r){
-        import std.algorithm:find, filter;
-        import std.array:array;
-        immutable first = r.length - r.find!(Cl).length;
-        immutable last = r.filter!(Cu).array.length-1;
-        return Tuple!(size_t, size_t)(first, last);
-    }
 }
