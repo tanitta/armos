822bffb7e06d3a2cac47d586a2ea0e45eaa2ed15
tanitta <ttata.trit@gmail.com>
2017-03-28 22:45:11 +0900

    Add primitives2d to examples.

---
+++ examples/primitives2d/source/app.d
@@ -0,0 +1,35 @@
+static import ar = armos;
+
+class TestApp : ar.app.BaseApp{
+    override void setup(){
+        _x = 0f;
+    }
+
+    override void update(){
+        _x += 2f;
+        if(_x > ar.app.currentWindow.size.x){
+            _x = 0f;
+        }
+    }
+
+    override void draw(){
+        ar.graphics.circlePrimitive(
+            ar.math.Vector3f(_x, 200, 0), // position
+            20                            // radius
+        ).drawFill;
+    }
+
+    private{
+        float _x;
+    }
+}
+
+void main(){
+    version(unittest){
+    }else{
+        ar.app.WindowConfig windowConfig = (new ar.app.WindowConfig())
+            .width(640)
+            .height(480);
+        ar.app.run(new TestApp, windowConfig);
+    }
+}
