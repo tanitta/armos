77460e902fd0c885ef424d99e75ddd7f75c1dcf1
tanitta <ttata.trit@gmail.com>
2017-08-16 23:20:14 +0900

    Enable to work midi input.

--- source/armos/communication/midi/package.d
+++ source/armos/communication/midi/package.d
@@ -3,25 +3,76 @@ module armos.communication.midi;
 import derelict.portmidi.portmidi;
 import derelict.portmidi.porttime;
 
+struct MidiMessage{
+    long timestamp;
+    long status;
+    long data1;
+    long data2;
+}
+
 /++
 +/
-class Stream {
+class MidiStream {
     public{
         static this(){
             DerelictPortMidi.load();
             DerelictPortTime.load();
         }
 
-        static auto inputStream(){
-            return new Stream;
+        static ~this(){
         }
 
+        static auto inputStream(in int id = defaultInputDeviceId(), in size_t bufferSize = 1024){
+            auto stream = new MidiStream;
+            auto e = Pm_OpenInput(&stream._stream,
+                                     id,
+                                     null,
+                                     bufferSize,
+                                     null,
+                                     null);
+            return stream;
+        }
+
+        static int defaultInputDeviceId(){
+            return Pm_GetDefaultInputDeviceID();
+        }
+
+        MidiStream close(){
+            Pm_Close(this._stream);
+            _hasClosed = true;
+            return this;
+        }
+
+        MidiMessage[] popMessages(){
+            immutable size_t max = 1024;
+            auto buffer = new PmEvent[](max);
+            auto numEvents = Pm_Read(_stream, buffer.ptr, max);
+            auto events = new MidiMessage[](numEvents);
+
+            foreach (size_t i, ref event; events) {
+                import std.conv:to;
+                event.timestamp= buffer[i].timestamp.to!long;
+                event.status = buffer[i].message.to!long & 0xFF;
+                event.data1= (buffer[i].message.to!long>>8) & 0xFF;
+                event.data2= (buffer[i].message.to!long>>16) & 0xFF;
+            }
+            return events;
+        }
+        // read(){return this;}
     }//public
 
     private{
         this(){}
+        ~this(){
+            if(!_hasClosed){
+                close;
+            }
+        }
 
         PmStream*  _stream;
         PmDeviceID _deviceId;
+        bool _hasClosed = false;
     }//private
 }//class Stream
+
+
