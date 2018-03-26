a7970a10c9b46318f4db739588ef1d1ad6a0fc49
tanitta <ttata.trit@gmail.com>
2018-03-26 23:06:19 +0900

    Add standardvertex example.

---
+++ examples/standardvertex/source/app.d
@@ -0,0 +1,57 @@
+static import ar = armos;
+// import armos.graphics;
+import std.stdio, std.math;
+
+alias V4 = ar.math.Vector4f;
+class TestApp : ar.app.BaseApp{
+    override void setup(){
+        _camera = (new ar.graphics.PerspCamera).position(ar.math.Vector3f(0, 0, 2.0))
+                                                 .target(ar.math.Vector3f.zero);
+        _indexBuffer = new ar.graphics.Buffer(ar.graphics.BufferType.ElementArray);
+        _positionBuffer = new ar.graphics.Buffer();
+        import armos.graphics.embeddedrenderer;
+
+    }
+
+    override void update(){
+        _counter += 0.1;
+        _camera.position = ar.math.Vector3f(5.0*cos(_counter*0.05),
+                                            0,
+                                            5.0*sin(_counter*0.05));
+        _positionBuffer.array([V4(-0.5, -0.5, 0, 1),
+                       V4(0.5, -0.5, 0, 1),
+                       V4(0, 0.5, 0, 1)]);
+        _indexBuffer.array([0, 1, 2]);
+    }
+
+    override void draw(){
+        import armos.graphics.renderer;
+        import armos.graphics.standardrenderer;
+        auto cr = armos.graphics.currentRenderer;
+        cr.backgroundColor(0.2, 0.2, 0.2, 1.).fillBackground;
+
+        cr.projectionMatrix(_camera.projectionMatrix)
+          .viewMatrix(_camera.viewMatrix)
+          .positions(_positionBuffer)
+          .indices(_indexBuffer)
+          .diffuse(1f-cos(_counter*0.5)*0.5f,
+                   1f-cos(_counter*0.51)*0.5f,
+                   1f-cos(_counter*0.52)*0.5f,
+                   1f)
+          .render();
+    }
+
+    private{
+        float _counter = 0f;
+        ar.graphics.Camera _camera;
+        ar.graphics.Buffer _indexBuffer;
+        ar.graphics.Buffer _positionBuffer;
+    }
+}
+
+void main(){
+    version(unittest){
+    }else{
+        ar.app.run(new TestApp);
+    }
+}
